8899674357802a4665b20dd0088353bc1304eaa8
Antonio Sucre <antonio.sucre@indikatore.com>
2021-12-26 18:25:55 -0300

    display all Categories

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
@@ -13,7 +14,7 @@ namespace BulkyBookWeb.Controllers
 
         public IActionResult Index()
         {
-            var categories = _db.Categories.ToList();
+            IEnumerable<Category> categories = _db.Categories;
             return View(categories);
         }
     }
