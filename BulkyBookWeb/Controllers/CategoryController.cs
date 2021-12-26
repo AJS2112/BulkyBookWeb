4991ba5ed6fbf0a3bf0ba1c794ea8e7c6da3b010
Antonio Sucre <antonio.sucre@indikatore.com>
2021-12-26 18:47:10 -0300

    add CreateCategory View

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -17,5 +17,10 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category> categories = _db.Categories;
             return View(categories);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
